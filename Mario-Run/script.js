ad26c97d0a6a2b8893a73714651e0919790f1d20
Devin <devin@getcraft.com>
2020-10-12 13:58:01 +0700

    feat: Add scoring function while mario is running
    - score will increment as long as mario is still alive
    - whenever user score a new record, will save the score into localStorage
    - when initial load, will check whether user has a highest record or no

--- Mario-Run/script.js
+++ Mario-Run/script.js
@@ -1,5 +1,11 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var highestScore = document.getElementById('highest-score');
+var score = document.getElementById('score');
+var highestScorePoint = Number(localStorage.getItem('mario-highest-score')) || 0;
+var currentPoint = 0;
+
+checkHighestScore();
 
 function jump() {
     if (character.classList != "animate") {
@@ -12,6 +18,11 @@ function jump() {
     }, 1000);
 }
 
+var incrementScore = setInterval(function () {
+    currentPoint++;
+    score.innerHTML = currentPoint;
+}, 100);
+
 var checkLost = setInterval(function () {
     var characterTop =
         parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -23,5 +34,19 @@ var checkLost = setInterval(function () {
         block.style.animation = "none";
         block.style.display = "none";
         alert("Your Lose -GameOver.");
+        clearInterval(incrementScore);
+        checkNewRecord();
     }
 }, 10);
+
+function checkHighestScore () {
+    if (highestScore) {
+        highestScore.innerHTML = highestScorePoint;
+    }
+}
+
+function checkNewRecord () {
+    if (currentPoint > highestScorePoint) {
+        localStorage.setItem('mario-highest-score', currentPoint);
+    }
+}
